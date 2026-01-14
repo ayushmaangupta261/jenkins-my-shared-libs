b22d0a893d551d9ebe272afb58f99b8ad704491c
Ayushmaan Gupta <132862778+ayushmaangupta261@users.noreply.github.com>
2026-01-14 17:49:32 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,16 +1,14 @@
-def call(String credId, String imageName){
-   withCredentials([
-            usernamePassword(
-                credentialsId: "dockerHubCreds",
-                passwordVariable: "dockerHubPass",
-                usernameVariable: "dockerHubUser"
-            )
-        ]) {
-
-            sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-            sh "docker image tag $imageName} ${dockerHubUser}/${imageName}"
-            sh "docker push ${dockerHubUser}/${imageName}"
-            echo "Uploaded to Docker Hub"
-
-        }  
+def call(String credId, String imageName) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: credId,
+            passwordVariable: 'dockerHubPass',
+            usernameVariable: 'dockerHubUser'
+        )
+    ]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker image tag ${imageName} ${dockerHubUser}/${imageName}"
+        sh "docker push ${dockerHubUser}/${imageName}"
+        echo "Uploaded to Docker Hub"
+    }
 }
